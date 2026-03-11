ac381ab10d64b50ccfa23dda1115880dc84217c6
Rahul Gangwar <rahulgangwar2625@gmail.com>
2026-03-11 23:18:22 +0530

    Prisma Setup

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import {PrismaClient} from "@prisma/client"
 import "./globals.css";
 
 const geistSans = localFont({
---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,13 @@
+
+// import styles from "./page.module.css";
+import {PrismaClient} from "@repo/db/client";
+const client = new PrismaClient();
+
+export default function Home() {
+  return (
+   <div className="text-2xl">
+
+    </div>
+    
+  );
+}
